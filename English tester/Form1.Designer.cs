d44ef128f7bd706b746b02245bb7a8d89412d540
Witmaster <Vladimyrischenko@gmail.com>
2015-09-27 15:37:17 +0300

    Release

---
+++ English tester/Form1.Designer.cs
@@ -0,0 +1,113 @@
+﻿namespace English_tester
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.RestartButton = new System.Windows.Forms.Button();
+            this.NextButton = new System.Windows.Forms.Button();
+            this.SkipButton = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(13, 29);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(259, 199);
+            this.textBox1.TabIndex = 0;
+            // 
+            // RestartButton
+            // 
+            this.RestartButton.Location = new System.Drawing.Point(13, 233);
+            this.RestartButton.Name = "RestartButton";
+            this.RestartButton.Size = new System.Drawing.Size(75, 23);
+            this.RestartButton.TabIndex = 1;
+            this.RestartButton.Text = "Start";
+            this.RestartButton.UseVisualStyleBackColor = true;
+            this.RestartButton.Click += new System.EventHandler(this.RestartButton_Click);
+            // 
+            // NextButton
+            // 
+            this.NextButton.Enabled = false;
+            this.NextButton.Location = new System.Drawing.Point(98, 234);
+            this.NextButton.Name = "NextButton";
+            this.NextButton.Size = new System.Drawing.Size(75, 23);
+            this.NextButton.TabIndex = 2;
+            this.NextButton.Text = "Next/Check";
+            this.NextButton.UseVisualStyleBackColor = true;
+            this.NextButton.Click += new System.EventHandler(this.NextButton_Click);
+            // 
+            // SkipButton
+            // 
+            this.SkipButton.Enabled = false;
+            this.SkipButton.Location = new System.Drawing.Point(183, 234);
+            this.SkipButton.Name = "SkipButton";
+            this.SkipButton.Size = new System.Drawing.Size(89, 23);
+            this.SkipButton.TabIndex = 3;
+            this.SkipButton.Text = "Skip";
+            this.SkipButton.UseVisualStyleBackColor = true;
+            this.SkipButton.Click += new System.EventHandler(this.SkipButton_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(272, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Enter the word pairs separated by ; , . : space or newline";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 262);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.SkipButton);
+            this.Controls.Add(this.NextButton);
+            this.Controls.Add(this.RestartButton);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button RestartButton;
+        private System.Windows.Forms.Button NextButton;
+        private System.Windows.Forms.Button SkipButton;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ English tester/Form1.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace English_tester
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private bool restart = true;
+        private string[] keys;
+        int index = -1;
+        private int score;
+        Dictionary<string, string> vocabulary = new Dictionary<string, string>();
+
+        private void RestartButton_Click(object sender, EventArgs e)
+        {
+            if (restart)
+            {
+                if (textBox1.Text.Split(new string[] { " ", Environment.NewLine, ";", ",", ".", ":" },
+                        StringSplitOptions.RemoveEmptyEntries).Length > 2)
+                {
+                    restart = false;
+                    index = -1;
+                    score = 0;
+                    label1.ForeColor = System.Drawing.Color.Black;
+                    RestartButton.Text = "New list";
+                    string[] temp = textBox1.Text.Split(new string[] { " ", Environment.NewLine, ";", ",", ".", ":" },
+                        StringSplitOptions.RemoveEmptyEntries);
+                    for (int i = 0; i < temp.Length; i += 2)
+                    {
+                        if (i + 1 < temp.Length)
+                        {
+                            vocabulary.Add(temp[i].ToLower(), temp[i + 1].ToLower());
+                        }
+                    }
+                         keys = vocabulary.Keys.ToArray<string>();
+                        Random rnd = new Random();
+                        for (int i = 1; i < keys.Length; i++)
+                        {
+                            int rndIndex = rnd.Next(i);
+                            if (rndIndex != i)
+                            {
+                                string tempstring = keys[rndIndex];
+                                keys[rndIndex] = keys[i];
+                                keys[i] = tempstring;
+                            }
+                        }
+                    NextButton.Enabled = true;
+                    SkipButton.Enabled = true;
+                    textBox1.Text = "";
+                    NextWord(false);
+                }
+                else
+                {
+                    label1.Text = "Enter the word pairs separated by ; , . : space or newline";
+                    label1.ForeColor = System.Drawing.Color.Red;
+                }
+            }
+            else
+            {
+                restart = true;
+                RestartButton.Text = "Begin";
+                NextButton.Enabled = false;
+                vocabulary = new Dictionary<string, string>();
+                index = -1;
+            }
+        }
+
+        private bool showResult = false;
+        private void NextWord(bool skip)
+        {
+            if (!skip)
+            {
+                if (showResult)
+                {
+                    if (vocabulary[keys[index]] == textBox1.Text.ToLower())
+                    {
+                        label1.Text = "Correct!";
+                        score++;
+                    }
+                    else
+                    {
+                        label1.Text = "Wrong!";
+                    }
+                    label1.ForeColor = System.Drawing.Color.Red;
+                    showResult = false;
+                }
+                else
+                {
+                    index++;
+                    showResult = true;
+                    label1.ForeColor = System.Drawing.Color.Black;
+                    label1.Text = keys[index];
+                    if (index == vocabulary.Count)
+                    {
+                        NextButton.Enabled = false;
+                        SkipButton.Enabled = false;
+                        label1.Text = "this was the last one. You scored " + score + " out of " +  index + " points";
+                    }
+                }
+            }
+            else
+            {
+                showResult = false;
+                NextWord(false);
+            }
+            textBox1.Text = "";
+        }
+
+        private void NextButton_Click(object sender, EventArgs e)
+        {
+            NextWord(false);
+        }
+
+        private void SkipButton_Click(object sender, EventArgs e)
+        {
+            NextWord(true);
+        }
+    }
+}
